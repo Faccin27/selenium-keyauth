0a35545118841f0b9e807d07fb40c20f7244d77d
Guilherme Faccin <gfaccin27@gmail.com>
2025-02-21 15:18:44 -0300

    feat: added users button selection

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ PASSWORD = os.getenv("pass")
 
 driver = Driver(uc=True)
 url = "https://keyauth.cc/login"
-driver.uc_open_with_reconnect(url, 10)
+driver.uc_open_with_reconnect(url, 2)
 driver.uc_gui_click_captcha()
 
-time.sleep(5)
+time.sleep(2)
 
 username_xpath = "/html/body/section/div/div[2]/div/form/div[3]/input"
 password_xpath = "/html/body/section/div/div[2]/div/form/div[4]/input"
 login_button_xpath = "/html/body/section/div/div[2]/div/form/button"
+users_button_xpath = "/html/body/div[1]/aside/div/div/div/div[3]/div[1]/ul/li/a"
 
 driver.find_element("xpath", username_xpath).send_keys(USERNAME)
 driver.find_element("xpath", password_xpath).send_keys(PASSWORD)
@@ -26,6 +27,9 @@ time.sleep(2)
 
 driver.find_element("xpath", login_button_xpath).click()
 
-time.sleep(100)
+time.sleep(2)
+
+driver.find_element("xpath", users_button_xpath).click()
+
 
 driver.quit()
