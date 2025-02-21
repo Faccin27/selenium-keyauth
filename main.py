e61cc1037257b72dcd6dfc22e0433b185a51442a
Guilherme Faccin <gfaccin27@gmail.com>
2025-02-21 15:24:37 -0300

    feat: searching users

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ username_xpath = "/html/body/section/div/div[2]/div/form/div[3]/input"
 password_xpath = "/html/body/section/div/div[2]/div/form/div[4]/input"
 login_button_xpath = "/html/body/section/div/div[2]/div/form/button"
 users_button_xpath = "/html/body/div[1]/aside/div/div/div/div[3]/div[1]/ul/li/a"
+search_input_xpath = "/html/body/div[1]/div[2]/main/div/div/div/div/div/div[12]/div/div[1]/div[2]/div/label/input"
+user = "Faccin"
 
 driver.find_element("xpath", username_xpath).send_keys(USERNAME)
 driver.find_element("xpath", password_xpath).send_keys(PASSWORD)
@@ -30,6 +32,11 @@ driver.find_element("xpath", login_button_xpath).click()
 time.sleep(2)
 
 driver.find_element("xpath", users_button_xpath).click()
+
+time.sleep(2)
+
+driver.find_element("xpath", search_input_xpath).send_keys(user)
+
 
 
 driver.quit()
